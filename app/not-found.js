8ecedea7e41878ae4ba58f8ea30fa6aee129810e
Greta Va <greta.vasilevskyte@gmail.com>
2023-05-22 16:50:54 +0200

    Add 'error', 'not-found' and 'loading' pages

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export const rootNotFoundMetadata = {
+  title: 'Not Found',
+  description: "Sorry, can't find the page you are looking for",
+};
+
+export default function RootNotFound() {
+  return <div>Sorry! This page was not found</div>;
+}
