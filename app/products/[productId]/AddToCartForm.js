c4335fa82e56e0f6975f02d0c579b8f777994d69
Greta Va <greta.vasilevskyte@gmail.com>
2023-05-26 15:57:20 +0200

    Add function 'AddToCartForm'

---
+++ app/products/[productId]/AddToCartForm.js
@@ -0,0 +1,32 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import updateQuantity from './actions';
+
+export default function AddToCartForm(props) {
+  const [quantity, setQuantity] = useState('1');
+  const router = useRouter();
+
+  return (
+    <form>
+      <input
+        type="number"
+        min={1}
+        max={15}
+        value={quantity}
+        onChange={(event) => {
+          setQuantity(event.currentTarget.value);
+        }}
+      />
+      <button
+        data-test-id="product-add-to-cart"
+        formAction={async () => {
+          router.refresh();
+          await updateQuantity(props.productId, quantity);
+        }}
+      >
+        Add to Cart
+      </button>
+    </form>
+  );
+}
