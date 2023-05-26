6d55fd1d26b1bddf92401fb62a269f09483e8c3e
Greta Va <greta.vasilevskyte@gmail.com>
2023-05-26 16:06:48 +0200

    Display products added to cart

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,3 +1,49 @@
+import Link from 'next/link';
+import { products } from '../../database/products';
+import { getCookie } from '../../util/cookies';
+import { parseJson } from '../../util/json';
+
+export const dynamic = 'force-dynamic';
+
 export default function CartPage() {
-  return <main>This is cart page</main>;
+  const productQuantityCookie = getCookie('cart');
+
+  const productQuantities = !productQuantityCookie
+    ? // cookie is undefined
+      // we need to create the new array with the productQuantity inside
+      []
+    : parseJson(productQuantityCookie);
+
+  const productsWithQuantities = products.map((product) => {
+    const matchingProductFromCookie = productQuantities.find(
+      (productObject) => product.id === productObject.id,
+    );
+
+    return {
+      ...product,
+      quantity: matchingProductFromCookie?.quantity,
+    };
+  });
+
+  // const productsInCart = productsWithQuantities.filter((item) => item.quantity);
+
+  // console.log(productsInCart);
+
+  return (
+    <main>
+      This is cart page
+      <div>
+        {productsWithQuantities.map((product) => {
+          return (
+            <div key={`product-${product.id}`}>
+              <Link href={`/products/${product.id}`}>
+                <div>{product.title}</div>
+              </Link>
+              {product.quantity}
+            </div>
+          );
+        })}
+      </div>
+    </main>
+  );
 }
