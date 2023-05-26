9df2091a1a5d4f3241d9e107ee0c7b2b3cb865dd
Greta Va <greta.vasilevskyte@gmail.com>
2023-05-26 15:54:29 +0200

    Add product description

--- database/products.js
+++ database/products.js
@@ -1,8 +1,36 @@
 export const products = [
-  { id: 1, title: 'Handmade soap', name: 'soap', price: '8.99' },
-  { id: 2, title: 'Water bottle', name: 'bottle', price: '25.99' },
-  { id: 3, title: 'Example1', name: 'tote', price: '15.99' },
-  { id: 4, title: 'Example2', name: 'shaver', price: '35.99' },
+  {
+    id: 1,
+    title: 'Handmade soap',
+    name: 'soap',
+    price: '8.99',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+  },
+  {
+    id: 2,
+    title: 'Water bottle',
+    name: 'bottle',
+    price: '25.99',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+  },
+  {
+    id: 3,
+    title: 'Example1',
+    name: 'tote',
+    price: '15.99',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+  },
+  {
+    id: 4,
+    title: 'Example2',
+    name: 'shaver',
+    price: '35.99',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+  },
 ];
 
 export function getProductById(id) {
