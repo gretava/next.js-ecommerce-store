2bd8b4a33d933bea157778d3f1d219eb75adf7a8
Greta Va <greta.vasilevskyte@gmail.com>
2023-06-01 18:00:03 +0200

    Add total price

--- app/cart/page.js
+++ app/cart/page.js
@@ -2,10 +2,14 @@ import Link from 'next/link';
 import { products } from '../../database/products';
 import { getCookie } from '../../util/cookies';
 import { parseJson } from '../../util/json';
+import RemoveProducts from './RemoveProducts';
+import UpdateItemQuantity from './UpdateItemQuantity';
 
 export const dynamic = 'force-dynamic';
 
 export default function CartPage() {
+  // const products = await lsProducts;
+
   const productQuantityCookie = getCookie('cart');
 
   const productQuantities = !productQuantityCookie
@@ -15,7 +19,9 @@ export default function CartPage() {
     : parseJson(productQuantityCookie);
 
   const productsWithQuantities = products.map((product) => {
+    // map() method is commonly used to apply some changes to the elements
     const matchingProductFromCookie = productQuantities.find(
+      // find() method returns the value of the first element that passes a test
       (productObject) => product.id === productObject.id,
     );
 
@@ -25,25 +31,47 @@ export default function CartPage() {
     };
   });
 
-  // const productsInCart = productsWithQuantities.filter((item) => item.quantity);
+  const itemsInCart = productsWithQuantities.filter((item) => item.quantity); // filter() creates a new array filled with elements that pass a test provided by a function
 
-  // console.log(productsInCart);
+  console.log(itemsInCart);
+
+  // /////// GET TOTAL PRICE /////////
+
+  const initialValue = 0;
+  const totalPrice = itemsInCart.reduce(
+    (accumulator, current) => accumulator + current.price * current.quantity,
+    initialValue,
+  );
+
+  console.log(totalPrice);
 
   return (
     <main>
-      This is cart page
+      <h1>Your cart</h1>
       <div>
-        {productsWithQuantities.map((product) => {
+        {itemsInCart.map((product) => {
           return (
             <div key={`product-${product.id}`}>
               <Link href={`/products/${product.id}`}>
                 <div>{product.title}</div>
               </Link>
-              {product.quantity}
+
+              <div>{product.price}</div>
+              <form>
+                <UpdateItemQuantity product={product} />
+              </form>
+              {/* <div>{product.quantity}</div> */}
+              <form>
+                <RemoveProducts product={product} />
+              </form>
             </div>
           );
         })}
       </div>
+      <br />
+      <br />
+
+      <div data-test-id="cart-total">Total price: {totalPrice}</div>
     </main>
   );
 }
