b0d2f1025908a165f9b9cda06da70d716982658d
Greta Va <greta.vasilevskyte@gmail.com>
2024-03-25 15:14:49 +0100

    Add if clause when cart is empty

--- app/cart/page.js
+++ app/cart/page.js
@@ -54,63 +54,74 @@ export default async function CartPage() {
   return (
     <main>
       <div className={styles.cartBody}>
-        <div className={styles.cartSections}>
-          <span>
-            <li>Product</li>
-          </span>
-          <li>Price</li>
-          <li>Quantity</li>
-          <li>Subtotal</li>
-        </div>
-        <div data-test-id="cart-product-<product id>">
-          {itemsInCart.map((product) => {
-            subTotal = product.quantity * product.price;
-            return (
-              <div
-                className={styles.cartProducts}
-                key={`product-${product.id}`}
-              >
-                <div className={styles.productTitleDiv}>
-                  <Link href={`/products/${product.id}`}>
-                    <Image
-                      className={styles.productImage}
-                      src={`/images/${product.name}.jpg`}
-                      width={150}
-                      height={180}
-                      alt={product.title}
-                    />
-                  </Link>
-                  <div className={styles.productTitle}>{product.title}</div>
-                </div>
-                <div>€{product.price}</div>
-                <form>
-                  <UpdateItemQuantity product={product} />
-                </form>
-                <div>€{subTotal.toFixed(2)}</div>
-                <div>
-                  <form>
-                    <RemoveProducts product={product} />
-                  </form>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        <div className={styles.totalCheckout}>
-          <div data-test-id="cart-total">
-            <span>Total price €{totalPrice.toFixed(2)}</span>
-          </div>
-          <div>
-            <Link
-              className={styles.checkoutButton}
-              type="button"
-              data-test-id="cart-checkout"
-              href="/checkout"
-            >
-              Checkout
+        {itemsInCart.length === 0 ? (
+          <div className={styles.emptyCartMessage}>
+            Your cart is empty. <br />
+            <Link href="/products" className={styles.checkoutButton}>
+              Shop now
             </Link>
           </div>
-        </div>
+        ) : (
+          <>
+            <div className={styles.cartSections}>
+              <span>
+                <li>Product</li>
+              </span>
+              <li>Price</li>
+              <li>Quantity</li>
+              <li>Subtotal</li>
+            </div>
+            <div data-test-id="cart-product-<product id>">
+              {itemsInCart.map((product) => {
+                subTotal = product.quantity * product.price;
+                return (
+                  <div
+                    className={styles.cartProducts}
+                    key={`product-${product.id}`}
+                  >
+                    <div className={styles.productTitleDiv}>
+                      <Link href={`/products/${product.id}`}>
+                        <Image
+                          className={styles.productImage}
+                          src={`/images/${product.name}.jpg`}
+                          width={150}
+                          height={180}
+                          alt={product.title}
+                        />
+                      </Link>
+                      <div className={styles.productTitle}>{product.title}</div>
+                    </div>
+                    <div>€{product.price}</div>
+                    <form>
+                      <UpdateItemQuantity product={product} />
+                    </form>
+                    <div>€{subTotal.toFixed(2)}</div>
+                    <div>
+                      <form>
+                        <RemoveProducts product={product} />
+                      </form>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <div className={styles.totalCheckout}>
+              <div data-test-id="cart-total">
+                <span>Total price €{totalPrice.toFixed(2)}</span>
+              </div>
+              <div>
+                <Link
+                  className={styles.checkoutButton}
+                  type="button"
+                  data-test-id="cart-checkout"
+                  href="/checkout"
+                >
+                  Checkout
+                </Link>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </main>
   );
